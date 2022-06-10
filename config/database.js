d2ab91d526dd82cce1c4f3eec779163bdcc3bbf8
RIO GERADI MUKE <riomuke3@gmail.com>
2022-06-10 19:16:10 +0700

    blm-selesai

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 const {
-  DB_USER = "postgres",
-  DB_PASSWORD = "riogaul123",
-  DB_NAME = "bcr",
-  DB_HOST = "127.0.0.1",
+  DB_USER = "csrmarxpaquubt",
+  DB_PASSWORD = "de6e0d0ea4b15f83a2c969d5dc920feee7c912b5efe10871b8dea3658515a7cb",
+  DB_NAME = "d93akmbt13sk2h",
+  DB_HOST = "ec2-52-3-2-245.compute-1.amazonaws.com",
   DB_PORT = "5432",
 } = process.env;
 
