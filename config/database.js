3aa05aba70a40c8d47ddafbec12c0a21575382f1
RIO GERADI MUKE <riomuke3@gmail.com>
2022-06-10 20:51:55 +0700

    baru deploy

--- config/database.js
+++ config/database.js
@@ -1,34 +1,58 @@
+require("dotenv").config();
+
 const {
-  DB_USER = "csrmarxpaquubt",
-  DB_PASSWORD = "de6e0d0ea4b15f83a2c969d5dc920feee7c912b5efe10871b8dea3658515a7cb",
-  DB_NAME = "d93akmbt13sk2h",
-  DB_HOST = "ec2-52-3-2-245.compute-1.amazonaws.com",
-  DB_PORT = "5432",
+  DB_USER = "",
+  DB_PASSWORD = "",
+  DB_NAME = "",
+  DB_HOST = "",
+  DB_PORT = "",
 } = process.env;
 
 module.exports = {
   development: {
     username: DB_USER,
     password: DB_PASSWORD,
-    database: `${DB_NAME}_development`,
+    database: DB_NAME,
     host: DB_HOST,
     port: DB_PORT,
-    dialect: "postgres"
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+
   },
   test: {
     username: DB_USER,
     password: DB_PASSWORD,
-    database: `${DB_NAME}_test`,
+    database: DB_NAME,
     host: DB_HOST,
     port: DB_PORT,
-    dialect: "postgres"
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+
   },
+
   production: {
     username: DB_USER,
     password: DB_PASSWORD,
-    database: `${DB_NAME}_production`,
+    database: DB_NAME,
     host: DB_HOST,
     port: DB_PORT,
-    dialect: "postgres"
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+
   }
 }
